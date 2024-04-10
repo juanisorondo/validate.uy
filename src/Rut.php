f726d9b66f6186c9053e20d48980e6184588818d
Lucas Azcue <lucasazcue97@gmail.com>
2024-04-10 16:00:16 -0300

    Update Rut.php
    [run ci]

--- src/Rut.php
+++ src/Rut.php
@@ -12,15 +12,15 @@ class Rut
         $rut = strval($rut);
 
         $primerosDosDigitos = intval(substr($rut, 0, 2));
+
         if ($primerosDosDigitos < 1 || $primerosDosDigitos > 21) {
             return false;
         }
 
-        if (substr($rut, 2, 6) === '000000') {
-            return false;
-        }
-        
-        if (substr($rut, 7, 2) !== '00') {
+        $digitos3Al8SonCeros = substr($rut, 2, 6) === '000000';
+        $digitos9Al10SonCeros = substr($rut, 8, 2) === '00';
+
+        if ($digitos3Al8SonCeros || !$digitos9Al10SonCeros) {
             return false;
         }
 
@@ -35,9 +35,8 @@ class Rut
         $digito_verificador = 11 - $tmp % 11;
         if ($digito_verificador == 11) {
             $digito_verificador = 0;
-        } elseif ($digito_verificador == 10) {
-            $digito_verificador = 1;
         }
+        
         return $rut[11] == $digito_verificador;
     }
 }
